a05c392c3e39f8340fdc1fa99b2fdc40204ffedb
juanlopera <juanlopera276@gmail.com>
2026-09-09 18:26:37 -0500

    feat: add moving average, z-score analysis, and plotting utilities

---
+++ src/plots.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def temperature_comparison_with_Rolling_7 (df):
+    
+    plt.figure (figsize=(12, 6))
+    plt.plot(
+        df["Date"],
+        df["AvgTemperature"],
+        label="Daily TEmperature",
+        marker="."
+    )
+    plt.plot(
+        df["Date"],
+        df["Rolling_7"],
+        label="7-Day moving average",
+    
+    )
+    plt.xlabel("Date")
+    plt.ylabel("Temperature (°F)")
+    plt.legend()
+    plt.show()
--- src/utils.py
+++ src/utils.py
@@ -11,3 +11,15 @@ def cities_comprobation(cities: list,df: pd.DataFrame):
         print(f"NAME : {city}")
         print(df_copy["State"].value_counts())
         print("-"*40)
+
+
+def z_score(df):
+
+    df_copy= df.copy()
+
+    mean_df = df_copy["AvgTemperature"].mean()
+    std_df= df_copy["AvgTemperature"].std()
+
+    df_copy["z-score"]=df_copy["AvgTemperature"].apply(lambda x: (x - mean_df)/std_df)
+
+    return df_copy[df["z-score"].apply(lambda x : abs(x)>3)]
